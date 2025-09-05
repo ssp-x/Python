dd7683ba96f79ebf2889acd70f0cc0199e0d8d61
sonny-xo <sspanyavong@gmail.com>
2025-09-05 12:26:34 -0400

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,18 +1,35 @@
 import random
+
 print("Let's play rock, paper, or scissors")
-player_choice = input("Choose rock, paper, or scissors: ").lower()
-choices = ["rock", "paper", "scissors"]
-computer_choice = random.choice(choices)
-print(f"Computer chose: {computer_choice}")
-if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
-  winner = "Player"
-elif player_choice == computer_choice:
-  winner = "Tie"
+
+player_wins = 0
+computer_wins = 0
+
+while player_wins < 2 and computer_wins < 2:
+  player_choice = input("\nChoose rock, paper, or scissors: ").lower()
+  choices = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(choices)
+  print(f"Computer chose: {computer_choice}")
+
+  if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
+    winner = "Player"
+  elif player_choice == computer_choice:
+    winner = "Tie"
+  else:
+    winner = "Computer"
+
+  if winner == "Player":
+    player_wins += 1
+    print("You won")
+  elif winner == "Computer":
+    computer_wins += 1
+    print("Computer won")
+  else:
+    print("It's a tie")
+
+  print(f"Current Score - Player: {player_wins}, Computer: {computer_wins}")
+
+if player_wins > computer_wins:
+  print("Congratulations! You won.")
 else:
-  winner = "Computer"
-if winner == "Player":
-  print("You won")
-elif winner == "Computer":
-  print("Computer won")
-else:
-  print("It's a tie")
+  print("Computer won!")
